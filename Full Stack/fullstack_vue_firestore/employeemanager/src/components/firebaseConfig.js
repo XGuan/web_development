131840733e3c451cfe0bdec7db9b53beb8d934f2
Xing Guan <xing1992@gmail.com>
2019-07-25 21:02:12 +0200

    initial commit

---
+++ Full Stack/fullstack_vue_firestore/employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+// the config from firebase should be here
+export default {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+}
